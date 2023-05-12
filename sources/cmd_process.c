9a947ec92c9e03c32bb39118cff819b48a8f1788
fyurtsev <furkan1yurtseven57@gmail.com>
2023-05-12 17:24:56 +0300

    done

--- sources/cmd_process.c
+++ sources/cmd_process.c
@@ -2,7 +2,9 @@
 
 int p_sleep(t_list *cmd) {
     // p_sleep fonksiyonunun işlevselliği
-    (void)cmd;
-    exit(1);
+    if(kill(cmd->cmd_act_input, 0) == 0)
+        printf("Process is active\n");
+    else
+        exit(1);
     return 0;
 }
